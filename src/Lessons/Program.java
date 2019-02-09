bce536803ec462d59132c110bfc22903f8b3ffa8
fudzya <fudzya@gmail.com>
2019-02-09 20:11:13 +0300

    Начата задача 2 по дз урока 3

--- src/Lessons/Program.java
+++ src/Lessons/Program.java
@@ -10,28 +10,28 @@ import java.util.Scanner;
 public class Program {
 
 	private static void guessNumberOfComputer() {
-		int attempt = 1;
+		int attempt = 3;
 		int max = 9;
 		Random random = new Random();
 		int numberOfComputer = random.nextInt(max + 1);
 		Scanner scanner = new Scanner(System.in);
-		System.out.println(String.format("Компьютер загадал число от 0 по %d, отгадайте его с 3 попыток", max));
-		while (attempt <= 3) {
+		System.out.println(String.format("Компьютер загадал число от 0 по %d, отгадайте его с %d попыток", max, attempt));
+		while (attempt >= 1) {
 			System.out.println("Введите число от 0 до " + max);
 			int numberOfUser = scanner.nextInt();
 			if (numberOfUser > numberOfComputer) {
 				System.out.println(String.format("Загаданное компьютером число меньше %d", numberOfUser));
-				attempt++;
+				attempt--;
 				continue;
 			}
 			if (numberOfUser < numberOfComputer) {
 				System.out.println(String.format("Загаданное компьютером число больше %d", numberOfUser));
-				attempt++;
+				attempt--;
 			} else {
 				break;
 			}
 		}
-		if (attempt > 3) {
+		if (attempt < 1) {
 			System.out.println("Вы не угaдали!");
 			answerToContinueGame();
 		} else {
@@ -51,8 +51,49 @@ public class Program {
 		scanner.close();
 	}
 
+	//private static String getWordOfComputer() {
+	//	String[] words = {
+	//			"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+	//			"cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
+	//			"pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+	//	Random random = new Random();
+	//	int indexOfWordOfComputer = random.nextInt(words.length);
+	//	return words[indexOfWordOfComputer];
+	//}
+
+	private static void guessWordOfComputer() {
+		String[] words = {
+				"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+				"cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
+				"pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+		Random random = new Random();
+		int indexOfWordOfComputer = random.nextInt(words.length);
+		String userWord = "###############";
+		//Scanner scanner = new Scanner(System.in);
+		//char litter = scanner.next().trim().charAt(0);
+		char[] charUserWorld = userWord.toCharArray();
+
+		for (int i = 0, j = 0; i < words[indexOfWordOfComputer].length(); i++, j++){
+			System.out.println("Введите букву");
+			Scanner scanner = new Scanner(System.in);
+			char litter = scanner.next().trim().charAt(0);
+			if(litter == words[indexOfWordOfComputer].trim().charAt(i)){
+				charUserWorld[j] = litter;
+				System.out.println(charUserWorld[j]);
+			}
+		}
+
+		//for (int i = 0, j = 0; i < (words[indexOfWordOfComputer]).length(); i++, j++){
+		//if (litter.equals(words[indexOfWordOfComputer].charAt(0))) {
+		//	charUserWorld[0] = litter.charAt(0);
+		//}
+		System.out.println(charUserWorld);
+	}
+
 	public static void main(String[] args) {
-		guessNumberOfComputer();
+		//guessNumberOfComputer();
+		guessWordOfComputer();
+
 	}
 }
 
