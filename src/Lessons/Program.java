670717d98d970c1c7446f46e06c5b87620c3a076
fudzya <fudzya@gmail.com>
2019-02-09 21:32:57 +0300

    Исправлен баг по задаче 2 по дз урока 3

--- src/Lessons/Program.java
+++ src/Lessons/Program.java
@@ -51,16 +51,6 @@ public class Program {
 		scanner.close();
 	}
 
-	//private static String getWordOfComputer() {
-	//	String[] words = {
-	//			"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
-	//			"cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
-	//			"pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-	//	Random random = new Random();
-	//	int indexOfWordOfComputer = random.nextInt(words.length);
-	//	return words[indexOfWordOfComputer];
-	//}
-
 	private static void guessWordOfComputer() {
 		String[] words = {
 				"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
@@ -71,22 +61,23 @@ public class Program {
 		String userWord = "###############";
 		char[] charUserWorld = userWord.toCharArray();
 
-		for (int i = 0, j = 0; i < words[indexOfWordOfComputer].length(); i++, j++) {
-			System.out.println("Введите букву");
+		System.out.println("Компьютер загадал продукт, отгадайте его!");
+		for (int i = 0, j = i; i < words[indexOfWordOfComputer].length(); i++, j = i) {
+			System.out.println("Введите " + (i + 1) + " букву");
 			Scanner scanner = new Scanner(System.in);
 			char litter = scanner.next().trim().charAt(0);
-			for (int x = 0; x < words[indexOfWordOfComputer].length(); x++) {
-				if (litter == words[indexOfWordOfComputer].trim().charAt(x)) {
-					charUserWorld[x] = litter;
-				}
-				else
-					charUserWorld[j] = words[indexOfWordOfComputer].charAt(i);
+			if (litter == words[indexOfWordOfComputer].charAt(i)) {
+				System.out.println("Введена верная буква!");
+				charUserWorld[i] = litter;
+			} else {
+				System.out.println("Введена неверна буква");
+				charUserWorld[j] = words[indexOfWordOfComputer].charAt(i);
 			}
-			System.out.println(charUserWorld);
-			
-		}
-		System.out.println(String.format("Игра окончена! Загаданное слово %s", words[indexOfWordOfComputer]));
 
+			System.out.println(charUserWorld);
+		}
+
+		System.out.println(String.format("Игра окончена! Загаданное слово %s", words[indexOfWordOfComputer]));
 	}
 
 	public static void main(String[] args) {
